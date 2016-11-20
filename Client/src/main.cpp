ff560221328e5f4bdec06ad7a4ac2653612b0463
pasteu_e <etienne.pasteur@epitech.eu>
2016-11-21 00:04:31 +0100

    Add SFML library to repo + Configure CMakeLists.txt to compile with SFML and add some nazi's flags

--- Client/src/main.cpp
+++ Client/src/main.cpp
@@ -2,6 +2,23 @@
 // Created by pasteu_e on 11/21/16.
 //
 
+#include <SFML/Graphics.hpp>
+
 int main() {
-    //Add usefull com tu push and create new branche
+    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+    return 0;
 }
