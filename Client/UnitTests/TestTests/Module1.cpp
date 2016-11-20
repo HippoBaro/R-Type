7318aa61e347efacc6a7bddb000ea2d9775c2240
Hippolyte Barraud <hippolyte.barraud@epitech.eu>
2016-11-20 17:09:13 +0100

    Fixes #15

---
+++ Client/UnitTests/TestTests/Module1.cpp
@@ -0,0 +1,13 @@
+//
+// Created by hippolyteb on 11/20/16.
+//
+
+#include <gtest/gtest.h>
+
+TEST(Client_Test_Module1, CanBindToValidEndpoint) {
+    ASSERT_EQ(1 * 2, 2) << "Should be equal to two !";
+}
+
+TEST(Client_Test_Module1, CanBindToInvalidEndpoint) {
+    ASSERT_EQ(1 * 2, 2) << "This will fail !";
+}
