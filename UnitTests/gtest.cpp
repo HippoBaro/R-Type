dbab2b326465b7856cc2ddc8baf684be64b5b143
Hippolyte Barraud <hippolyte.barraud@epitech.eu>
2016-11-20 03:20:00 +0100

    Provided google test unit testing example.

---
+++ UnitTests/gtest.cpp
@@ -0,0 +1,10 @@
+//
+// Created by hippolyteb on 11/20/16.
+//
+
+#include <gtest/gtest.h>
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
