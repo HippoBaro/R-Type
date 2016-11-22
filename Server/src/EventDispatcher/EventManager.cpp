21bd88b027fd75fd9ac9c5c0cbfcd9aec7af78c2
aguado_e <enzo.aguado@epitech.eu>
2016-11-22 15:17:56 +0100

    move definitions in separate files

--- Server/src/EventDispatcher/EventManager.cpp
+++ Server/src/EventDispatcher/EventManager.cpp
@@ -3,3 +3,16 @@
 //
 
 #include "EventManager.hpp"
+
+RType::EventManager::EventManager() {}
+
+void RType::EventManager::Register(RType::Event event, std::function<void(RType::IEntity &)> callback)
+{
+  _listeners[event].push_back(callback);
+}
+
+void RType::EventManager::Emit(RType::Event event, RType::IEntity &data)
+{
+  for (auto i : _listeners[event])
+    i(data);
+}
--- Server/src/EventDispatcher/EventManager.hpp
+++ Server/src/EventDispatcher/EventManager.hpp
@@ -13,53 +13,20 @@
 
 namespace RType {
 
-  class IEntity {
-  public:
-    IEntity(int a) : a(a){}
-    int a;
-  };
-
-  class Bullet : public IEntity {
-  public:
-    Bullet(int a) : IEntity(a) {}
-  };
-
-
+  // TODO: Put the real IEntity definition
+  class IEntity {};
 
   class EventManager {
 
   private:
-    std::map<RType::Event, std::vector<std::function<void(IEntity&)>>> _listeners;
+    std::map<RType::Event, std::vector<std::function<void(IEntity&)>>> _listeners = {};
 
   public:
-    EventManager() {}
+    EventManager();
 
-    void Register(RType::Event event, std::function<void(IEntity&)> callback) {
-      _listeners[event].push_back(callback);
-    }
-
-    void Emit(RType::Event event, IEntity& data) {
-      for (auto i : _listeners[event]) {
-        i(data);
-      }
-    }
+    void Register(RType::Event event, std::function<void(IEntity&)> callback);
+    void Emit(RType::Event event, IEntity& data);
   };
-
-  class EventListener {
-  private:
-    std::shared_ptr<RType::EventManager> _eventManager;
-
-  public:
-    EventListener(std::shared_ptr<RType::EventManager> eventManager) : _eventManager(eventManager) {}
-
-    template <typename EventType>
-    void Register(RType::Event event, std::function<void(EventType&)> callback) {
-      _eventManager->Register(event, [callback](IEntity& data) {
-        callback(static_cast<EventType&>(data));
-      });
-    }
-  };
-
 }
 
 
