1b864e6a5ac1f2a10b31b9d18956b96d8effdb82
Hippolyte Barraud <hippolyte.barraud@epitech.eu>
2016-11-23 10:50:27 +0100

    Enable Timer to reference future origin.

--- Shared/Time/Timer.cpp
+++ Shared/Time/Timer.cpp
@@ -4,17 +4,16 @@
 
 #include "Timer.hpp"
 
-Timer::Timer() : _origin() {}
-
-void Timer::Reset() {
-    _origin = std::chrono::duration_cast<std::chrono::milliseconds >(std::chrono::system_clock::now().time_since_epoch());
-}
-
-const std::chrono::milliseconds &Timer::getOrigin() const {
+const std::chrono::time_point<std::chrono::system_clock> &Timer::getOrigin() const {
     return _origin;
 }
 
 const TimeRef Timer::getCurrent() {
-    auto now = std::chrono::duration_cast<std::chrono::milliseconds >(std::chrono::system_clock::now().time_since_epoch());
-    return TimeRef(now - _origin);
+    auto now = std::chrono::time_point_cast<std::chrono::milliseconds>(std::chrono::system_clock::now());
+    auto test = now - std::chrono::time_point_cast<std::chrono::milliseconds>(_origin);
+    return TimeRef(test);
+}
+
+Timer::Timer(std::chrono::time_point<std::chrono::system_clock> origin) : _origin(origin) {
+
 }
