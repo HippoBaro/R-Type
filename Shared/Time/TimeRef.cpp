f5c6cb8137d339a87fdb4c2983332a9b555a12f6
Hippolyte Barraud <hippolyte.barraud@epitech.eu>
2016-11-24 13:48:37 +0100

    Added unit test for Time abstraction

--- Shared/Time/TimeRef.cpp
+++ Shared/Time/TimeRef.cpp
@@ -5,14 +5,38 @@
 #include <chrono>
 #include "TimeRef.hpp"
 
-TimeRef::TimeRef() : _current(std::chrono::milliseconds()) {
+TimeRef::TimeRef() : _current(std::chrono::milliseconds()) { }
 
-}
-
-TimeRef::TimeRef(const std::chrono::milliseconds &milliseconds) : _current(milliseconds) {
-
-}
+TimeRef::TimeRef(const std::chrono::milliseconds &milliseconds) : _current(milliseconds) { }
 
 const std::chrono::milliseconds &TimeRef::getMilliseconds() const {
     return _current;
+}
+
+bool operator<(const TimeRef &lhs, const TimeRef &rhs) {
+    return lhs._current < rhs._current;
+}
+
+bool operator>(const TimeRef &lhs, const TimeRef &rhs) {
+    return rhs < lhs;
+}
+
+bool operator<=(const TimeRef &lhs, const TimeRef &rhs) {
+    return !(rhs < lhs);
+}
+
+bool operator>=(const TimeRef &lhs, const TimeRef &rhs) {
+    return !(lhs < rhs);
+}
+
+bool operator==(const TimeRef &lhs, const TimeRef &rhs) {
+    return lhs._current == rhs._current;
+}
+
+bool operator!=(const TimeRef &lhs, const TimeRef &rhs) {
+    return !(rhs == lhs);
+}
+
+TimeRef TimeRef::GetRelative(const std::chrono::milliseconds &milliseconds) {
+    return TimeRef(_current + milliseconds);
 }
---
+++ Shared/UnitTests/Time/TimeRef.cpp
@@ -0,0 +1,29 @@
+//
+// Created by hippolyteb on 11/20/16.
+//
+
+#include <gtest/gtest.h>
+#include <thread>
+#include "../../Time/TimeRef.hpp"
+#include "../../Time/Timer.hpp"
+
+TEST(Tests_TimeRef, AddTimeRef) {
+    auto timer = Timer(std::chrono::system_clock::now());
+    auto now = timer.getCurrent();
+    auto future = now.GetRelative(std::chrono::seconds(10));
+    ASSERT_EQ(future.getMilliseconds().count() >= 10000 && future.getMilliseconds().count() < 10100, true) << "TimeRef getMilliseconds() should be 10000 but is " << future.getMilliseconds().count();
+}
+
+TEST(Tests_TimeRef, SubstractTimeRef) {
+    auto now = TimeRef(std::chrono::seconds(10));
+    auto past = now.GetRelative(std::chrono::seconds(-10));
+    ASSERT_EQ(past.getMilliseconds().count() >= 0 && past.getMilliseconds().count() < 100, true) << "TimeRef getMilliseconds() should be 0 but is " << past.getMilliseconds().count();
+}
+
+TEST(Tests_TimeRef, CompareTimeRef) {
+    auto now = TimeRef(std::chrono::seconds(10));
+    auto past = now.GetRelative(std::chrono::seconds(-10));
+    ASSERT_EQ(now > past, true) << "now should be > to past";
+    auto now2 = past.GetRelative(std::chrono::seconds(10));
+    ASSERT_EQ(now2 == now, true) << "now2 should be == to now";
+}
