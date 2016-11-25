db47a718b2ea8ead11692c93367ea62a60aba7b9
Hippolyte Barraud <hippolyte.barraud@epitech.eu>
2016-11-25 17:15:28 +0100

    Variadic external lib ctor enabled. But they don't share same memory page. Pointer don't pass.

--- SharedLibs/Entities/Monsters/DummyMonster/DummyMonster.cpp
+++ SharedLibs/Entities/Monsters/DummyMonster/DummyMonster.cpp
@@ -4,7 +4,7 @@
 
 #include "DummyMonster.hpp"
 
-DummyMonster::DummyMonster(std::shared_ptr<Timer> &timer) : _timer(timer), _partition(EntityPartition().AddSegment(
+DummyMonster::DummyMonster(Timer *timer) : _timer(timer), _partition(EntityPartition().AddSegment(
         PartitionSegmentBuilder()
                 .Begins(_timer->getCurrent())
                 .Ends(_timer->getCurrent().GetRelative(std::chrono::seconds(1)))
@@ -15,8 +15,10 @@ DummyMonster::DummyMonster(std::shared_ptr<Timer> &timer) : _timer(timer), _part
 
 }
 
-extern "C" DLLEXPORT DummyMonster *create(std::shared_ptr<Timer> &timer) {
-    return new DummyMonster(timer);
+extern "C" DLLEXPORT DummyMonster *create(const std::initializer_list<void *> &init) {
+    for (auto i : init) // access by value, the type of i is int
+        return new DummyMonster((Timer *)i);
+    return new DummyMonster((Timer *) nullptr);
 }
 
 extern "C" DLLEXPORT void destroy(DummyMonster *p) {
