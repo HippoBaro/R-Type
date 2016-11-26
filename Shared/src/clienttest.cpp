4679ede8d85e513570bf357804c1332406a2de85
barre_k <kevin.barre@epitech.eu>
2016-11-26 19:57:38 +0100

    linux end

--- Shared/src/clienttest.cpp
+++ Shared/src/clienttest.cpp
@@ -4,13 +4,25 @@
 
 #include "RTypeSocket.hpp"
 
-int main()
-{
-    RTypeSocket client =  RTypeSocket();
+int main() {
+    RTypeSocket client = RTypeSocket();
+    bool gamecondition = true;
+    char lol[] = "wala ca marche";
+    char *messageofserver;
+
 
     client.InitConnection("localhost");
-    client.WriteServer("dimitri");
-    client.CRun();
-
+    client.WriteServer("joueur numero 1");
+    client.WriteServer("nique");
+    client.WriteServer(lol);
+    while (gamecondition)
+    {
+        messageofserver = client.ListenServer();
+        if (messageofserver != nullptr)
+        {
+            std::cout << "message of server " << messageofserver << " " << std::string(messageofserver) << std::endl;
+        }
+    }
+    client.EndConnection();
     return 0;
 }
--- Shared/src/servertest.cpp
+++ Shared/src/servertest.cpp
@@ -6,9 +6,24 @@
 
 int main()
 {
+    char str[] = "azertyuiopqsdfghjklmwxcvbn AZERTYUIOPQSDFGHJKLMWXCVBN";
+    char *ptr;
+    ptr = str;
     RTypeSocket server =  RTypeSocket();
+    char *messageofclient;
+    bool gamecondition = true;
 
     server.InitConnection();
-    server.SRun();
+    while (gamecondition)
+    {
+        messageofclient = server.ListenClient();
+        if (messageofclient != nullptr)
+        {
+            std::cout << "messageofclient " << messageofclient << std::endl;
+
+            server.SendMessageToAllClients("message recu");
+        }
+    }
+    server.EndConnection();
     return 0;
 }
