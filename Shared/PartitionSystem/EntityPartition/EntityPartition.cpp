52d80639bb4f5cd8e926a54435a40e1142ecf728
Hippolyte Barraud <hippolyte.barraud@epitech.eu>
2016-11-26 03:26:45 +0100

    Entity partition working !

--- Shared/PartitionSystem/EntityPartition/EntityPartition.cpp
+++ Shared/PartitionSystem/EntityPartition/EntityPartition.cpp
@@ -2,6 +2,7 @@
 // Created by hippolyteb on 11/25/16.
 //
 
+#include <algorithm>
 #include "EntityPartition.hpp"
 
 EntityPartition::EntityPartition() : _segments(std::vector<PartitionSegment>()) {}
@@ -15,4 +16,14 @@ EntityPartition &EntityPartition::Repeat(int const count) {
     for (int i = 0; i < count - 1; ++i)
         AddSegment(_segments.back());
     return *this;
+}
+
+PartitionSegment EntityPartition::GetCurrentSegment(TimeRef const &timeRef) {
+    auto ret = *std::find_if(_segments.begin(), _segments.end(), [timeRef](PartitionSegment &arg) {
+        return arg.isPartOf(timeRef);
+    });
+
+    if (ret.isPartOf(timeRef))
+        return ret;
+    return _segments.back();
 }
