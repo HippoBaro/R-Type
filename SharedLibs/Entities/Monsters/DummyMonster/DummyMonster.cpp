ba7f7d1c4fc7975b9d6f111ed73217f0d25c51f0
Hippolyte Barraud <hippolyte.barraud@epitech.eu>
2016-11-26 19:27:29 +0100

    Modified way to add PartitionSegments to enable more fluent syntax.

--- SharedLibs/Entities/Monsters/DummyMonster/DummyMonster.cpp
+++ SharedLibs/Entities/Monsters/DummyMonster/DummyMonster.cpp
@@ -6,27 +6,25 @@
 #include <thread>
 #include "DummyMonster.hpp"
 
-DummyMonster::DummyMonster(Timer *timer) : _timer(timer), _partition() {
-    _partition = EntityPartition().AddSegment(
+DummyMonster::DummyMonster(Timer *timer) : _timer(timer), _partition(timer) {
+    _partition.AddSegment(
             PartitionSegmentBuilder()
                 .Begins(_timer->getCurrent())
                 .Ends(_timer->getCurrent().GetRelative(std::chrono::seconds(1)))
                 .From(vec2d(0, 0))
                 .To(vec2d(10, 10))
-                .Build(_timer)
-    ).AddSegment(
+    ).ContinueWith(
             PartitionSegmentBuilder()
                 .Begins(_timer->getCurrent().GetRelative(std::chrono::seconds(1)))
                 .Ends(_timer->getCurrent().GetRelative(std::chrono::seconds(10)))
                 .From(vec2d(9, 9))
                 .To(vec2d(5, 5))
-                .Build(_timer)
-    );
+    ).Repeat(5);
 
-    for (int i = 0; i < 111; ++i) {
+    for (int i = 0; i < 501; ++i) {
         std::this_thread::sleep_for(std::chrono::milliseconds(100));
         auto const current = _partition.GetCurrentSegment(timer->getCurrent()).getLocationVector().GetTweened();
-        std::cout << "Monster pos : [X:" << current.x << "; Y:" << current.y << "]" << std::endl;
+        std::cout << "Monster pos : [X:" << current.x << "; Y:" << current.y << "] TIME : " << _timer->getCurrent().getMilliseconds().count() << std::endl;
     }
 }
 
