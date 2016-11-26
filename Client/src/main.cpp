cfed223eaa42b37c02c77578a2906abe63b45e17
pasteu_e <etienne.pasteur@epitech.eu>
2016-11-26 20:05:03 +0100

    Init branch

--- Client/src/main.cpp
+++ Client/src/main.cpp
@@ -2,22 +2,7 @@
 // Created by pasteu_e on 11/21/16.
 //
 
-#include <SFML/Graphics.hpp>
-
 int main() {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
-
-    while (window.isOpen()) {
-        sf::Event event;
-        while (window.pollEvent(event)) {
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
-        window.clear();
-        window.draw(shape);
-        window.display();
-    }
+    //Init branch #36 -> Mettre en place l'architecture client
     return 0;
 }
