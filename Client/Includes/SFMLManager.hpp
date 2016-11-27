561e5a65d4996071528099a1a4b026c1b3f5932a
pasteu_e <etienne.pasteur@epitech.eu>
2016-11-27 22:11:45 +0100

    Mise en place de l'architecture global du client

--- Client/Includes/SFMLManager.hpp
+++ Client/Includes/SFMLManager.hpp
@@ -5,20 +5,15 @@
 #ifndef R_TYPE_SFMLMANAGER_HPP
 #define R_TYPE_SFMLMANAGER_HPP
 
-#include <SFML/Window.hpp>
-#include <SFML/Graphics.hpp>
-#include "IRtypeDrawingContext.hpp"
-#include "RTypeInputListener.hpp"
 
-constexpr int Width = 800;
-constexpr int Height = 600;
+#include <IRtypeDrawingContext.hpp>
+#include "RTypeInputListener.hpp"
 
 class SFMLManager {
 private:
     std::unique_ptr<RTypeInputListener> _inputListener;
     std::unique_ptr<IRtypeDrawingContext> _gameContext;
     std::unique_ptr<IRtypeDrawingContext> _menuContext;
-    sf::RenderWindow _window;
 
 public:
     SFMLManager();
