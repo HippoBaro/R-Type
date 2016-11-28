6e757a78c1af6c65e5c49ea081202159f9a4598c
pasteu_e <etienne.pasteur@epitech.eu>
2016-11-28 01:32:17 +0100

    Adding InputListener and create Window in SFMLManager

--- Client/Includes/SFMLManager.hpp
+++ Client/Includes/SFMLManager.hpp
@@ -5,15 +5,20 @@
 #ifndef R_TYPE_SFMLMANAGER_HPP
 #define R_TYPE_SFMLMANAGER_HPP
 
-
-#include <IRtypeDrawingContext.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
+#include "IRtypeDrawingContext.hpp"
 #include "RTypeInputListener.hpp"
+
+#define WIDTH 800
+#define HEIGHT 600
 
 class SFMLManager {
 private:
     std::unique_ptr<RTypeInputListener> _inputListener;
     std::unique_ptr<IRtypeDrawingContext> _gameContext;
     std::unique_ptr<IRtypeDrawingContext> _menuContext;
+    sf::RenderWindow _window;
 
 public:
     SFMLManager();
