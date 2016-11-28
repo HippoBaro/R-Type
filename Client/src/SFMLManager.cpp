310f74423cd414221f85a5bc342252a49f8ea7db
pasteu_e <etienne.pasteur@epitech.eu>
2016-11-28 17:21:25 +0100

    add texture and exemple to display sprite

--- Client/src/SFMLManager.cpp
+++ Client/src/SFMLManager.cpp
@@ -20,7 +20,13 @@ void SFMLManager::Run() {
     // Boucle de jeu.
     while (_window.isOpen()) {
         _inputListener->CheckForInputs(_window);
-        _window.clear(sf::Color::Black);
+//        sf::Texture texture;
+//        texture.loadFromFile("sprites/r-typesheet1.gif", sf::IntRect(101, 3, 32, 14));
+//        sf::Sprite sprite;
+//        sprite.setTexture(texture);
+//        sprite.setPosition(sf::Vector2f(10, 50));
+//        sprite.setScale(sf::Vector2f(10.f, 10.f));
+//        _window.draw(sprite);
         _window.display();
     }
     //std::this_thread::sleep_until(std::chrono::system_clock::now() + std::chrono::milliseconds(500));
