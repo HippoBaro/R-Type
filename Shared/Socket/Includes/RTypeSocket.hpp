d6a3c2cbbe76ae9841d9b0861e797f770a11852f
pasteu_e <etienne.pasteur@epitech.eu>
2016-11-30 01:02:18 +0100

    Linux socket done ! + Unit test for Socket :)

--- Shared/Socket/Includes/RTypeSocket.hpp
+++ Shared/Socket/Includes/RTypeSocket.hpp
@@ -5,8 +5,39 @@
 #ifndef R_TYPE_RTYPESOCKET_HPP
 #define R_TYPE_RTYPESOCKET_HPP
 
+#include "IRTypeSocket.hpp"
+#include <strings.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <fcntl.h>
+#include <arpa/inet.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <malloc.h>
+#include "RTypeNetworkPayload.h"
 
-class RTypeSocket {
+constexpr uint16_t port() { return 5673; }
+
+class RTypeSocket : public IRTypeSocket {
+private:
+    int _socket;
+    struct sockaddr_in _addr;
+
+private:
+    void CreateSocket();
+
+public:
+    RTypeSocket();
+
+    RTypeSocket(const std::string &addr);
+
+    virtual ~RTypeSocket();
+
+    void Bind() override final;
+
+    void Receive(RTypeNetworkPayload &payload, size_t length) override final;
+
+    void Send(const std::string &payload) override final;
 
 };
 
