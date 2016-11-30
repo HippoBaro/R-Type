cf75ea2327432fd36b83ad5c67e642825aeb2177
Hippolyte Barraud <hippolyte.barraud@epitech.eu>
2016-12-01 00:54:17 +0100

    Enabled UnitTests on AppVeyor ! All platforms are now unittested before every PR !

--- Shared/UnitTests/Time/Timer.cpp
+++ Shared/UnitTests/Time/Timer.cpp
@@ -7,28 +7,22 @@
 #include <Timer.hpp>
 
 TEST(Tests_Time, CreateTimerFromCurrentTime) {
-    auto thread = new std::thread([&] {
-        auto timer = Timer(std::chrono::system_clock::now());
-        std::this_thread::sleep_for(std::chrono::seconds(2));
-        auto now = timer.getCurrent().getMilliseconds();
-        ASSERT_EQ(now.count() > 1900 || now.count() < 2100, true) << "Timer getMilliseconds() should be 2000 but is " << now.count();
-    });
-    thread->join();
+    auto timer = Timer(std::chrono::system_clock::now());
+    std::this_thread::sleep_for(std::chrono::seconds(2));
+    auto now = timer.getCurrent().getMilliseconds();
+    ASSERT_EQ(now.count() > 1900 || now.count() < 2100, true) << "Timer getMilliseconds() should be 2000 but is " << now.count();
 }
 
 TEST(Tests_Time, CreateTimerFromFutureTime) {
-    auto thread = new std::thread([&] {
-        auto nowClock = std::chrono::system_clock::now();
-        auto dtn = std::chrono::duration<int>(std::chrono::seconds(2));
-        auto future = nowClock + dtn;
+    auto nowClock = std::chrono::system_clock::now();
+    auto dtn = std::chrono::duration<int>(std::chrono::seconds(2));
+    auto future = nowClock + dtn;
 
-        auto timer = Timer(future);
-        std::this_thread::sleep_for(std::chrono::seconds(2));
-        auto now = timer.getCurrent().getMilliseconds();
-        ASSERT_EQ(now.count() <= 1, true) << "Timer getMilliseconds() should be 0 but is " << now.count();
-        std::this_thread::sleep_for(std::chrono::seconds(2));
-        now = timer.getCurrent().getMilliseconds();
-        ASSERT_EQ(now.count() > 1900 && now.count() < 2100, true) << "Timer getMilliseconds() should be 2000 but is " << now.count();
-    });
-    thread->join();
+    auto timer = Timer(future);
+    std::this_thread::sleep_for(std::chrono::seconds(2));
+    auto now = timer.getCurrent().getMilliseconds();
+    ASSERT_EQ(now.count() <= 1, true) << "Timer getMilliseconds() should be 0 but is " << now.count();
+    std::this_thread::sleep_for(std::chrono::seconds(2));
+    now = timer.getCurrent().getMilliseconds();
+    ASSERT_EQ(now.count() > 1900 && now.count() < 2100, true) << "Timer getMilliseconds() should be 2000 but is " << now.count();
 }
