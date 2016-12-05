eba4ed23d726fc3671c586367863f34dbdfcda19
aguado_e <enzo.aguado@epitech.eu>
2016-12-05 23:01:55 +0100

    WIP refactor way of handling serialization

---
+++ Shared/Serializer/DeserializeBuffer.cpp
@@ -0,0 +1,5 @@
+//
+// Created by aguado_e on 12/5/16.
+//
+
+#include "DeserializeBuffer.hpp"
---
+++ Shared/Serializer/DeserializeBuffer.hpp
@@ -0,0 +1,18 @@
+//
+// Created by aguado_e on 12/5/16.
+//
+
+#ifndef R_TYPE_DESERIALIZEBUFFER_HPP
+#define R_TYPE_DESERIALIZEBUFFER_HPP
+
+
+#include "ISerializerBuffer.hpp"
+
+namespace RType {
+  class DeserializeBuffer : public ISerializerBuffer {
+
+  };
+}
+
+
+#endif //R_TYPE_DESERIALIZEBUFFER_HPP
