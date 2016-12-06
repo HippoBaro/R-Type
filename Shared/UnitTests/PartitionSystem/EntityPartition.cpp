89e49b1f0c396c09070997b0fb293c83c75bc661
Hippolyte Barraud <hippolyte.barrau@epitech.eu>
2016-12-06 15:56:04 +0100

    Fixed EntityPartition unitTest for OSX and activated verbose unittesting

--- Shared/UnitTests/PartitionSystem/EntityPartition.cpp
+++ Shared/UnitTests/PartitionSystem/EntityPartition.cpp
@@ -28,16 +28,16 @@ TEST(EntityPartionTest, PlayPlayValidEntityPartition) {
 
     std::this_thread::sleep_for(std::chrono::milliseconds(500));
     current = partition.GetCurrentSegment(timer->getCurrent()).getLocationVector().GetTweened();
-    ASSERT_EQ(current.x <= 20 && current.x >= 19.8 &&
-              current.y <= 20 && current.y >= 19.8, true) << "Partition wasn't played correctly. Vec was :" << current.x;
+    ASSERT_EQ(current.x <= 20 && current.x >= 19.5 &&
+              current.y <= 20 && current.y >= 19.5, true) << "Partition wasn't played correctly. Vec was :" << current.x;
 
     std::this_thread::sleep_for(std::chrono::milliseconds(250));
     current = partition.GetCurrentSegment(timer->getCurrent()).getLocationVector().GetTweened();
-    ASSERT_EQ(current.x <= 13.2 && current.x >= 12.3 &&
-              current.y <= 13.2 && current.y >= 12.3, true) << "Partition wasn't played correctly. Vec was :" << current.x;
+    ASSERT_EQ(current.x <= 12.5 && current.x >= 12 &&
+              current.y <= 12.5 && current.y >= 12, true) << "Partition wasn't played correctly. Vec was :" << current.x;
 
     std::this_thread::sleep_for(std::chrono::milliseconds(250));
     current = partition.GetCurrentSegment(timer->getCurrent()).getLocationVector().GetTweened();
-    ASSERT_EQ(current.x <= 5.2 && current.x >= 4.8 &&
-              current.y <= 5.2 && current.y >= 4.8, true) << "Partition wasn't played correctly. Vec was :" << current.x;
+    ASSERT_EQ(current.x <= 5.5 && current.x >= 5 &&
+              current.y <= 5.5 && current.y >= 5, true) << "Partition wasn't played correctly. Vec was :" << current.x;
 }
