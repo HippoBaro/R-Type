ebbea1772fe0ad4cd3b53a919f896b7ce21a931e
Hippolyte Barraud <hippolyte.barraud@epitech.eu>
2016-12-07 18:05:45 +0100

    EventManager header-only

--- Shared/Include/EventDispatcher/EventManager.hpp
+++ Shared/Include/EventDispatcher/EventManager.hpp
@@ -38,12 +38,22 @@ namespace RType {
                 void *, IMessage *message)>>>>> _listeners = {};
 
     public:
-        EventManager();
+        EventManager() {};
 
         void
-        AddListener(std::shared_ptr<std::map<RType::Event, std::vector<std::function<void(void *, IMessage *message)>>>> &callbacks);
+        AddListener(std::shared_ptr<std::map<RType::Event, std::vector<std::function<void(void *, IMessage *message)>>>> &callbacks){
+            _listeners.push_back(callbacks);
+        }
 
-        void Emit(RType::Event event, IMessage *message, void *sender);
+        void Emit(RType::Event event, IMessage *message, void *sender) {
+            for (auto const callbacklist : _listeners) {
+                if (callbacklist != nullptr)
+                    for (auto callback : (*callbacklist)[event])
+                        callback(sender, message);
+            }
+            if (message)
+                delete message;
+        }
     };
 }
 
