45f6e47e988cf70bc5c7aff6c9ca50d02547414f
Hippolyte Barraud <hippolyte.barraud@epitech.eu>
2016-12-07 17:13:19 +0100

    Refactored EventDispatcher to accept arbitrary sender type

--- Shared/EventDispatcher/EventManager.cpp
+++ Shared/EventDispatcher/EventManager.cpp
@@ -6,11 +6,11 @@
 
 RType::EventManager::EventManager() {}
 
-void RType::EventManager::AddListener(std::shared_ptr<std::map<RType::Event, std::vector<std::function<void(Entity*, IMessage *message)>>>> &callbacks) {
+void RType::EventManager::AddListener(std::shared_ptr<std::map<RType::Event, std::vector<std::function<void(void *, IMessage *message)>>>> &callbacks) {
     _listeners.push_back(callbacks);
 }
 
-void RType::EventManager::Emit(RType::Event event, IMessage *message, Entity *sender) {
+void RType::EventManager::Emit(RType::Event event, IMessage *message, void *sender) {
     for (auto const callbacklist : _listeners) {
         if (callbacklist != nullptr)
             for (auto callback : (*callbacklist)[event])
