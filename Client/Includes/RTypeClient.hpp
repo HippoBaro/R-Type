e84dc60a05bcc0bd28eb21ec00c9307251310b6a
pasteu_e <etienne.pasteur@epitech.eu>
2016-12-08 15:43:32 +0100

    Fix Wrong include in Socket module and implement RTypeNetworkClient

--- Client/Includes/RTypeClient.hpp
+++ Client/Includes/RTypeClient.hpp
@@ -13,7 +13,7 @@
 class RTypeClient {
 private:
     std::shared_ptr<RType::EventManager> _eventManager = std::shared_ptr<RType::EventManager>(new RType::EventManager());
-    std::unique_ptr<RTypeNetworkClient> _networkClient = std::unique_ptr<RTypeNetworkClient>(new RTypeNetworkClient());
+    std::unique_ptr<RTypeNetworkClient> _networkClient = std::unique_ptr<RTypeNetworkClient>(new RTypeNetworkClient(_eventManager));
     std::unique_ptr<SFMLManager> _sfmlManager = std::unique_ptr<SFMLManager>(new SFMLManager(_eventManager));
 
 public:
--- Client/src/RTypeNetworkClient.cpp
+++ Client/src/RTypeNetworkClient.cpp
@@ -2,12 +2,28 @@
 // Created by pasteu_e on 27/11/16.
 //
 
+#include <Messages/UserInputMessage.hpp>
 #include "RTypeNetworkClient.hpp"
 
-RTypeNetworkClient::RTypeNetworkClient() {
-
-}
-
-void RTypeNetworkClient::Run() {
-
+RTypeNetworkClient::RTypeNetworkClient(std::shared_ptr<RType::EventManager> eventManager) : _eventManager(eventManager), _eventListener(eventManager){
+    _eventListener.Subscribe<Entity, UserInputMessage>(UserInputMessage::EventType, [&](Entity *, UserInputMessage *message) {
+        switch (message->getEventType()) {
+            case CLOSE_WINDOWS:
+                break;
+            case USER_UP:
+                break;
+            case USER_DOWN:
+                break;
+            case USER_RIGHT:
+                break;
+            case USER_LEFT:
+                break;
+            case USER_SPACE:
+                break;
+            case USER_ENTER:
+                break;
+            case USER_ESCAPE:
+                break;
+        }
+    });
 }
