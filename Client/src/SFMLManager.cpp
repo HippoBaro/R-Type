be59529b5daa0c5b132379a59c830abab6ef48b1
pasteu_e <etienne.pasteur@epitech.eu>
2016-12-08 23:11:54 +0100

    Gros rebase sa maman :'(

--- Client/src/SFMLManager.cpp
+++ Client/src/SFMLManager.cpp
@@ -19,10 +19,10 @@ SFMLManager::SFMLManager(std::shared_ptr<RType::EventManager> eventManager) : _i
 }
 
 void SFMLManager::Run() {
-    //_soundManager->PlayMusic(true, "sprites/menuBackground.ogg");
+    _soundManager->PlayMusic(true, "sprites/menuBackground.ogg");
     sf::VideoMode desktop =  sf::VideoMode::getDesktopMode();
     _window.create(sf::VideoMode(Width, Height, desktop.bitsPerPixel), "R-Type");
-
+    glEnable(GL_TEXTURE_2D);
     _window.setVerticalSyncEnabled(true);
     _window.setFramerateLimit(60);
 
@@ -31,8 +31,7 @@ void SFMLManager::Run() {
     // Boucle de jeu.
     while (_window.isOpen()) {
         _inputListener->CheckForInputs(_window);
-
-        _menuContext->Draw(context);
+        _menuContext->Draw(context, _textureBag);
         //_gameContext->Draw(context, _textureBag);
         renderSprite.setTexture(context.getTexture());
         _window.draw(renderSprite);
