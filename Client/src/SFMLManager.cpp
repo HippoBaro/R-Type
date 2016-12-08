2073ab858c9cdafffd88adbab9a26d422d2aeadf
pasteu_e <etienne.pasteur@epitech.eu>
2016-12-08 01:22:18 +0100

    Ajout SoundManager + Plein de choses pour le menu (animation etc.)

--- Client/src/SFMLManager.cpp
+++ Client/src/SFMLManager.cpp
@@ -6,29 +6,28 @@
 #include <iostream>
 #include "RTypeMenuContext.hpp"
 #include "SFMLManager.hpp"
-#include "RTypeGameContext.hpp"
+//#include "RTypeGameContext.hpp"
 #include <SFML/OpenGL.hpp>
 
-SFMLManager::SFMLManager() : _inputListener(new RTypeInputListener()), _gameContext(new RTypeGameContext()), _menuContext(new RTypeMenuContext()) {
+SFMLManager::SFMLManager() : _inputListener(new RTypeInputListener()), _gameContext(nullptr), _menuContext(new RTypeMenuContext()), _soundManager(new SoundManager()) {
 
 }
 
 void SFMLManager::Run() {
-    sf::RenderTexture context;
-    sf::Sprite renderSprite;
-
+    _soundManager->PlayMusic(true, "sprites/menuBackground.ogg");
     sf::VideoMode desktop =  sf::VideoMode::getDesktopMode();
     sf::RenderWindow _window(sf::VideoMode(Width, Height, desktop.bitsPerPixel), "R-Type");
-    glEnable(GL_TEXTURE_2D);
+
     _window.setVerticalSyncEnabled(true);
     _window.setFramerateLimit(60);
+
+    sf::RenderTexture context;
+    sf::Sprite renderSprite;
     // Boucle de jeu.
     while (_window.isOpen()) {
         _inputListener->CheckForInputs(_window);
-
         _menuContext->Draw(context);
         //_gameContext->Draw(context);
-
         renderSprite.setTexture(context.getTexture());
         _window.draw(renderSprite);
         _window.display();
