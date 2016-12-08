782486d236a51ee9d7cbf6b9cf00400ba5d65a2e
pasteu_e <etienne.pasteur@epitech.eu>
2016-12-08 15:43:32 +0100

    Fix Wrong include in Socket module and implement RTypeNetworkClient

--- Client/Includes/RTypeNetworkClient.hpp
+++ Client/Includes/RTypeNetworkClient.hpp
@@ -6,10 +6,20 @@
 #define R_TYPE_RTYPENETWORKCLIENT_HPP
 
 
+#include <memory>
+#include <Socket/IRTypeSocket.hpp>
+#include <Socket/UNIX/RTypeSocket.hpp>
+#include <Socket/Enum/RTypeSocketType.h>
+#include <EventDispatcher/EventListener.hpp>
+
 class RTypeNetworkClient {
+private:
+    std::unique_ptr<IRTypeSocket> _networkClient = std::unique_ptr<IRTypeSocket>(new RTypeSocket<TCP>("127.0.0.1", 8769));
+    std::shared_ptr<RType::EventManager> _eventManager;
+    RType::EventListener _eventListener;
+
 public:
-    RTypeNetworkClient();
-    void Run();
+    RTypeNetworkClient(std::shared_ptr<RType::EventManager> eventManager);
 };
 
 
