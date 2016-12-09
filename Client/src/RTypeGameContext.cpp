007617b9fad740fdee78602ada5b1e9b0ee7a5b6
Hippolyte Barraud <hippolyte.barraud@epitech.eu>
2016-12-09 21:43:31 +0100

    Partition now loaded from json

--- Client/src/RTypeGameContext.cpp
+++ Client/src/RTypeGameContext.cpp
@@ -10,14 +10,32 @@
 RTypeGameContext::RTypeGameContext() : _pool() {
     _timer = std::make_shared<Timer>(std::chrono::steady_clock::now());
     _pool = std::make_shared<ClientEntityPool>(_timer);
-    _pool->LoadPartition("");
-    //auto stratPos = vec2<float>(0, 0);
-
-    //_pool->AddEntity("DummyMonster", stratPos);
+    _pool->LoadPartition("{\n"
+                         "\t\"partitionName\":\"TestPartition\",\n"
+                         "\t\"backgroundTheme\":\"test.ogg\",\n"
+                         "\t\"backgroundEntity\": \n"
+                         "\t\t{\n"
+                         "\t\t\t\"entityName\":\"testEntity\",\n"
+                         "\t\t\t\"startPosition\": {\n"
+                         "\t\t\t\t\"x\":0,\n"
+                         "\t\t\t\t\"y\":0\n"
+                         "\t\t}\n"
+                         "\t},\n"
+                         "\t\"entities\":[\n"
+                         "\t\t{\n"
+                         "\t\t\t\"entityName\":\"DummyMonster\",\n"
+                         "\t\t\t\"startPosition\": {\n"
+                         "\t\t\t\t\"x\":0,\n"
+                         "\t\t\t\t\"y\":0\n"
+                         "\t\t\t},\n"
+                         "\t\t\t\"startTime\":1000\n"
+                         "\t\t}\n"
+                         "\t]\n"
+                         "}");
 }
 
 void RTypeGameContext::Draw(sf::RenderTexture &context, TextureBag &bag) {
-    context.clear(sf::Color::White);
+    context.clear(sf::Color::Black);
     _pool->ProcessEntities();
     _pool->Draw(context, bag);
     context.display();
--- Shared/EntityPool/EntityPool.cpp
+++ Shared/EntityPool/EntityPool.cpp
@@ -48,30 +48,7 @@ void EntityPool::SpawnProjectile(FireProjectileMessage const &message) {
 void EntityPool::LoadPartition(std::string const &partition) {
     RType::json j;
 
-    auto json = "{\n"
-            "\t\"partitionName\":\"TestPartition\",\n"
-            "\t\"backgroundTheme\":\"test.ogg\",\n"
-            "\t\"backgroundEntity\": \n"
-            "\t\t{\n"
-            "\t\t\t\"entityName\":\"testEntity\",\n"
-            "\t\t\t\"startPosition\": {\n"
-            "\t\t\t\t\"x\":0,\n"
-            "\t\t\t\t\"y\":0\n"
-            "\t\t}\n"
-            "\t},\n"
-            "\t\"entities\":[\n"
-            "\t\t{\n"
-            "\t\t\t\"entityName\":\"DummyMonster\",\n"
-            "\t\t\t\"startPosition\": {\n"
-            "\t\t\t\t\"x\":0,\n"
-            "\t\t\t\t\"y\":0\n"
-            "\t\t\t},\n"
-            "\t\t\t\"startTime\":1000\n"
-            "\t\t}\n"
-            "\t]\n"
-            "}";
-
-    j = RType::json::parse(json);
+    j = RType::json::parse(partition);
     for (auto const &i : j["entities"]) {
         std::string name = i["entityName"];
         vec2<float> startPos(i["startPosition"]["x"], i["startPosition"]["y"]);
