cf32bfebe96328c225eb258e908388f0f7be4ee2
aguado_e <enzo.aguado@epitech.eu>
2016-12-12 00:17:28 +0100

    cross plateform endianess swap

--- Shared/Include/Serializer/SerializationHelper.hpp
+++ Shared/Include/Serializer/SerializationHelper.hpp
@@ -7,10 +7,25 @@
 
 #include <iostream>
 #include <assert.h>
+#include <climits>
 
 namespace RType {
   namespace SerializationHelper {
     bool _IsBigEndian();
+
+    template <typename T>
+    T SwapEndian(T u)
+    {
+      static_assert (CHAR_BIT == 8, "Char size != 8. Can't perform serialization.");
+      union {
+        T u;
+        unsigned char u8[sizeof(T)];
+      } source, dest;
+      source.u = u;
+      for (size_t k = 0; k < sizeof(T); k++)
+        dest.u8[k] = source.u8[sizeof(T) - k - 1];
+      return dest.u;
+    }
 
     template <typename Ttype>
     void Serialize(char *buffer, uint16_t index, Ttype & value)
@@ -20,13 +35,13 @@ namespace RType {
           *((Ttype*)(buffer + index)) = value;
 
         else if (sizeof(Ttype) == 2) // 2 bytes
-          *((Ttype*)(buffer + index)) = __bswap_16(value);
+          *((Ttype*)(buffer + index)) = SwapEndian(value);
 
         else if (sizeof(Ttype) == 4) // 4 bytes
-          *((Ttype*)(buffer + index)) = __bswap_32(value);
+          *((Ttype*)(buffer + index)) = SwapEndian(value);
 
         else if (sizeof(Ttype) == 8) // 8 bytes
-          *((Ttype*)(buffer + index)) = __bswap_64(value);
+          *((Ttype*)(buffer + index)) = SwapEndian(value);
       }
       else
         *((Ttype*)(buffer + index)) = value;
