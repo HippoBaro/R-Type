5bd67a408a8cfa62ea4bc94b820b3ba4df5a8c16
Hippolyte Barraud <hippolyte.barraud@epitech.eu>
2016-12-13 02:29:00 +0100

    PartitionSegment now fully support serialization. Not tested yet.

--- Shared/Include/Serializer/ISerializable.hpp
+++ Shared/Include/Serializer/ISerializable.hpp
@@ -5,16 +5,18 @@
 #ifndef R_TYPE_ISERIALIZABLE_HPP
 #define R_TYPE_ISERIALIZABLE_HPP
 
-#include "Packer.hpp"
-
 namespace RType {
 
-  class ISerializable {
-  public:
-    virtual ~ISerializable() {};
+    class Packer;
 
-    virtual void Serialize(RType::Packer & packer) = 0;
-  };
+    class ISerializable {
+    public:
+        virtual ~ISerializable() {};
+
+        virtual void Serialize(RType::Packer &packer) = 0;
+    };
 }
+
+#include "Packer.hpp"
 
 #endif //R_TYPE_ISERIALIZABLE_HPP
