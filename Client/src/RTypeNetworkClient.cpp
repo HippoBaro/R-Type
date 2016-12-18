98d8857b1abef17cb4bec9f4aa268082d275c13c
Hippolyte Barraud <hippolyte.barraud@epitech.eu>
2016-12-19 00:26:10 +0100

    Rerouted received message to ClientEntityPool. Still need to refactor lot's of stuff & introduce some intermediary transactions before deserialization can occur.

--- Client/src/RTypeNetworkClient.cpp
+++ Client/src/RTypeNetworkClient.cpp
@@ -5,6 +5,7 @@
 #include <Messages/UserInputMessage.hpp>
 #include <Entities/Entity.hpp>
 #include <Messages/ReceiveNetworkPayloadMessage.hpp>
+#include <Messages/ReceivedNetworkPayloadMessage.hpp>
 #include "RTypeNetworkClient.hpp"
 
 RTypeNetworkClient::RTypeNetworkClient(std::shared_ptr<RType::EventManager> &eventManager) : _eventManager(eventManager), _eventListener(eventManager) {
@@ -16,7 +17,8 @@ RTypeNetworkClient::RTypeNetworkClient(std::shared_ptr<RType::EventManager> &eve
     _eventListener.Subscribe<Entity, ReceiveNetworkPayloadMessage>(ReceiveNetworkPayloadMessage::EventType, [&](Entity *, ReceiveNetworkPayloadMessage *message) {
         RTypeNetworkPayload payload;
         while (_networkGameClient->Receive(payload)) {
-            std::cout << "Received stuff" << std::endl;
+            //std::cout << "Received stuff" << std::endl;
+            _eventManager->Emit(ReceivedNetworkPayloadMessage::EventType, new ReceivedNetworkPayloadMessage(payload), this);
         }
     });
 }
