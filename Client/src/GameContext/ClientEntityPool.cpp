9dff94c363b1917c33636c35b3eeded4456593f7
Hippolyte Barraud <hippolyte.barraud@epitech.eu>
2016-12-20 23:59:17 +0100

    Ok, client now mirror server, still some work on projectiles, though

--- Client/src/GameContext/ClientEntityPool.cpp
+++ Client/src/GameContext/ClientEntityPool.cpp
@@ -28,7 +28,7 @@ void ClientEntityPool::AddEntity(std::string const &entityName, uint16_t id, vec
     auto pos = initialPos;
     ManagedExternalInstance<Entity> entity(ExternalClassFactoryLoader::Instance->GetInstanceOf<Entity>("", "Drawable" + entityName, { &id, &_timer, &_eventManager, &now, &pos, params }, "createDrawable", "destroyDrawable"));
     _factory.RegisterEntityType(entity->getTypeId(), "Drawable" + entityName);
-    //EntityPool::AddEntity(entity);
+    EntityPool::AddEntity(entity);
 }
 
 ClientEntityPool::ClientEntityPool(const std::shared_ptr<Timer> &timer,
