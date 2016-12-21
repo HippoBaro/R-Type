a3af3b6d9368d6ff06c61b8a91971268e5c5e2e3
Hippolyte Barraud <hippolyte.barraud@epitech.eu>
2016-12-21 17:07:26 +0100

    Overridden partition loading on client

--- SharedLibs/Entities/Monsters/DummyMonster/DummyMonster.cpp
+++ SharedLibs/Entities/Monsters/DummyMonster/DummyMonster.cpp
@@ -22,7 +22,7 @@ DummyMonster::DummyMonster(uint16_t id, std::shared_ptr<Timer> timer, std::share
                             .Begins(timeRef)
                             .For(std::chrono::seconds(10000))
                             .Translate(vec2<float>(0, 0))
-                            .Fire("SimpleProjectile", 0.5))
+                            .Fire("SimpleProjectile", 5))
             /*.AddSegment(PartitionSegmentBuilder()
                                 .For(std::chrono::seconds(5))
                                 .Translate(vec2<float>(-400, -400))
