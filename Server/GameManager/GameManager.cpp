9dc33ca6a0a7b2fe82b0126ca03246dc5dcb4ab9
Hippolyte Barraud <hippolyte.barraud@epitech.eu>
2016-12-21 22:28:22 +0100

    Fixed windows cast & type name

--- Server/GameManager/GameManager.cpp
+++ Server/GameManager/GameManager.cpp
@@ -3,11 +3,12 @@
 //
 
 #include "GameManager/GameManager.hpp"
+#include <chrono>
 
 void GameManager::Start() {
     _networkManager.Start();
     auto temp = std::vector<PlayerRef>();
     _gameInstanceManager.CreateInstance(temp);
 
-    std::this_thread::sleep_until(std::chrono::system_clock::now() + std::chrono::hours(std::numeric_limits<int>::max()));
+    std::this_thread::sleep_until(std::chrono::system_clock::now() + std::chrono::hours((std::numeric_limits<int>::max)()));
 }
