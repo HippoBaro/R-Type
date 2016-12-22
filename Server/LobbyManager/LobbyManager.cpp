a6fade31d8454149296956b4021770f2310b6ce1
pasteu_e <etienne.pasteur@epitech.eu>
2016-12-22 02:26:07 +0100

    Server work without thread.

--- Server/LobbyManager/LobbyManager.cpp
+++ Server/LobbyManager/LobbyManager.cpp
@@ -8,7 +8,7 @@
 void LobbyManager::Start() {
     auto sub = RType::EventListener(_eventManager);
     sub.Subscribe<void, NewClientConnectionMessage>(NewClientConnectionMessage::EventType, [&](void *sender, NewClientConnectionMessage *message) {
-        _clients.push_back(message->getClient());
+        _clients[_i++] = message->getClient();
     });
     Run();
 }
@@ -17,8 +17,8 @@ void LobbyManager::Run() {
     std::shared_ptr<IRTypeSocket> tmp;
     while (true) {
         _networkManager.IsThereNewClient();
-        while (!_clients.try_dequeue(tmp));
-        _networkManager.AskClientForRoomName(tmp);
+        _networkManager.CheckForIncomingMessage(_clients);
+        std::cout << _clients.size() << std::endl;
     }
 }
 
