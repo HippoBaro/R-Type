9a2dd0e4b274364106e63dcbef1e9445e1bc21b4
Hippolyte Barraud <hippolyte.barraud@epitech.eu>
2016-12-23 13:38:25 +0100

    Added EntityPacker as a factory for creating entity from packet.

--- Shared/Include/EntityFactory/EntityFactory.hpp
+++ Shared/Include/EntityFactory/EntityFactory.hpp
@@ -17,27 +17,6 @@ public:
     EntityFactory() : _typesRef() {}
 
 public:
-    ManagedExternalInstance<Entity> CreateFromPayload(RType::Packer &packer, std::shared_ptr<Timer> &timer, std::shared_ptr<RType::EventManager> &eventManager) {
-        long time = 0;
-        packer.Pack(time);
-
-        uint16_t entityType = 0;
-        packer.Pack(entityType);
-
-        uint16_t id = 0;
-        packer.Pack(id);
-
-        TimeRef now = 0;
-        vec2<float> pos;
-
-		if (_typesRef.count(entityType) == 0)
-			throw new std::runtime_error("Requested entity type is not registered.");
-
-        timer->RecalibrateOrigin(time);
-        auto entity = ManagedExternalInstance<Entity>(ExternalClassFactoryLoader::Instance->GetInstanceOf<Entity>("", _typesRef[entityType], { &id, &timer , &eventManager, &now, &pos }, "create", "destroy"));
-        entity->Serialize(packer);
-        return entity;
-    }
 
     void RegisterEntityType(uint16_t const &typeId, std::string const &entityName){
         _typesRef[typeId] = entityName;
@@ -53,6 +32,12 @@ public:
         uint16_t id;
         auto entity = ManagedExternalInstance<Entity>(ExternalClassFactoryLoader::Instance->GetInstanceOf<Entity>("", entityName, { &id, &timer , &eventManager, &now, &pos }, "create", "destroy"));
         RegisterEntityType(entity, entityName);
+    }
+
+    std::string &getTypeFromTypeId(uint16_t id){
+        if (_typesRef.count(id) == 0)
+            throw new std::runtime_error("Requested entity type is not registered.");
+        return _typesRef[id];
     }
 };
 
---
+++ Shared/Include/EntityPacker/EntityPacker.hpp
@@ -0,0 +1,61 @@
+//
+// Created by hippolyteb on 12/23/16.
+//
+
+#ifndef R_TYPE_ENTITYPACKER_HPP
+#define R_TYPE_ENTITYPACKER_HPP
+
+#include <Serializer/Packer.hpp>
+#include <LibraryLoader/ManagedExternalInstance.hpp>
+#include <Entities/Entity.hpp>
+#include <LibraryLoader/ExternalClassFactoryLoader.hpp>
+#include <EntityFactory/EntityFactory.hpp>
+
+class EntityPacker {
+private:
+    RType::Packer _packer;
+    EntityFactory _factory;
+
+    std::string _type;
+    uint16_t _typeId;
+
+    long _timeStamp;
+
+    uint16_t _entityId;
+
+
+public:
+    EntityPacker(const RType::Packer &packer, const EntityFactory &factory) : _packer(packer), _factory(factory) {
+        _packer.Pack(_timeStamp);
+        _packer.Pack(_typeId);
+        _packer.Pack(_entityId);
+        _type = _factory.getTypeFromTypeId(_typeId);
+    }
+
+    ManagedExternalInstance<Entity> GetEntity(std::shared_ptr<Timer> &timer, std::shared_ptr<RType::EventManager> &eventManager) {
+        TimeRef now = 0;
+        vec2<float> pos;
+
+        auto entity = ManagedExternalInstance<Entity>(ExternalClassFactoryLoader::Instance->GetInstanceOf<Entity>("", _type, { &_entityId, &timer , &eventManager, &now, &pos }, "create", "destroy"));
+        entity->Serialize(_packer);
+        return entity;
+    }
+
+    const std::string &getType() const {
+        return _type;
+    }
+
+    uint16_t getTypeId() const {
+        return _typeId;
+    }
+
+    long getTimeStamp() const {
+        return _timeStamp;
+    }
+
+    uint16_t getEntityId() const {
+        return _entityId;
+    }
+};
+
+#endif //R_TYPE_ENTITYPACKER_HPP
