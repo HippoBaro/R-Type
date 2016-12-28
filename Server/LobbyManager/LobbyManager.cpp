4bd7220f857b8165fe9220350a3d01ae25f31eb9
HippoBaro <hippolyte.barraud@epitech.eu>
2016-12-29 00:58:09 +0100

    added test

--- Server/LobbyManager/LobbyManager.cpp
+++ Server/LobbyManager/LobbyManager.cpp
@@ -19,6 +19,7 @@ void LobbyManager::Start() {
 
     //Event listener pour ajouter des messages a envoyer sur le reseau
     sub.Subscribe<void, SendTCPNetworkPayloadMessage>(SendTCPNetworkPayloadMessage::EventType, [&](void *sender, SendTCPNetworkPayloadMessage *message) {
+        std::cout << "Before adding to queue : " << message->getPacker().getBuffer() << std::endl << "Len : " << message->getPacker().getLength() << std::endl << std::endl;
         _toSend.push_back(std::make_pair(message->getDestination(), message->ConvertToSocketMessage()));
     });
 
@@ -93,7 +94,8 @@ void LobbyManager::CheckInstance() {
 void LobbyManager::SendToClients() {
     if (_toSend.size() != 0) {
         for (auto it = _toSend.begin(); it != _toSend.end();) {
-            if (_networkManager.SendOverTCP(it->second, it->first, 100)) {
+            //std::cout << "Before adding to queue : " << message->getPacker().getBuffer() << std::endl << "Len : " << message->getPacker().getLength() << std::endl << std::endl;
+            if (_networkManager.SendOverTCP(it->second, it->first, -1)) {
                 it = _toSend.erase(it);
             } else {
                 ++it;
