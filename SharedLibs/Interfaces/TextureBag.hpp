58f9df91d0e02385d0832b7b7a43349fb3a388f0
HippoBaro <hippolyte.barraud@epitech.eu>
2016-12-29 22:43:02 +0100

    moving textureBag to ptr semantics

--- SharedLibs/Interfaces/TextureBag.hpp
+++ SharedLibs/Interfaces/TextureBag.hpp
@@ -12,30 +12,30 @@
 
 class TextureBag {
 private:
-    std::map<std::string, sf::Texture> _textures;
+    std::map<std::string, std::shared_ptr<const sf::Texture>> _textures;
 
 public:
-    TextureBag() : _textures() {};
+    TextureBag() : _textures() {}
 
 public:
-    sf::Texture *getTexture(std::string const &textureFile, sf::IntRect const &rect){
+    std::shared_ptr<const sf::Texture> getTexture(std::string const &textureFile, sf::IntRect const &rect) {
         std::stringstream ss;
 
         ss << textureFile << rect.height << rect.left << rect.top << rect.width;
 
         auto key = ss.str();
         if (_textures.count(key))
-            return &_textures[key];
+            return _textures[key];
         return nullptr;
     }
 
-    sf::Texture *AddTexture(std::string const &textureFile, sf::IntRect const &rect, sf::Texture const &texture) {
+    std::shared_ptr<const sf::Texture> &AddTexture(std::string const &textureFile, sf::IntRect const &rect, std::shared_ptr<const sf::Texture> const &texture) {
         std::stringstream ss;
 
         ss << textureFile << rect.height << rect.left << rect.top << rect.width;
         auto key = ss.str();
         _textures[key] = texture;
-        return &_textures[key];
+        return _textures[key];
     }
 };
 
