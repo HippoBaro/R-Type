bba8c9f9d2ea2f671847aa3729e98f5ea2bc91e3
aguado_e <enzo.aguado@epitech.eu>
2016-12-30 22:45:27 +0100

    fix badly copied char*

--- Shared/Include/Socket/RTypeNetworkPayload.h
+++ Shared/Include/Socket/RTypeNetworkPayload.h
@@ -12,23 +12,26 @@ class RTypeNetworkPayload {
 private:
     class PackerContent {
     public:
-        char *Buffer = nullptr;
+        char *_buffer = nullptr;
         bool _shouldDelete = false;
 
-        PackerContent(int lenght) : Buffer(new char[lenght]), _shouldDelete(true) {}
-        PackerContent(char *data) : Buffer(data) {}
+        PackerContent(int length) : _buffer(new char[length]), _shouldDelete(true) {}
 
-        PackerContent(const PackerContent& other) : Buffer(other.Buffer), _shouldDelete(other._shouldDelete) {}
+        PackerContent(char *data, int length) : _buffer(new char[length]), _shouldDelete(true) {
+            memcpy(_buffer, data, (size_t)length);
+        }
+
+        PackerContent(const PackerContent& other) : _buffer(other._buffer), _shouldDelete(other._shouldDelete) {}
 
         PackerContent& operator=(const PackerContent& other) {
-            Buffer = other.Buffer;
+            _buffer = other._buffer;
             _shouldDelete = other._shouldDelete;
             return *this;
         }
 
         virtual ~PackerContent() {
             if (_shouldDelete)
-                delete[](Buffer);
+                delete[](_buffer);
         }
     };
 
@@ -41,26 +44,26 @@ public:
 
     RTypeNetworkPayload() : _payload(), Length() { }
 
-    RTypeNetworkPayload(char *payload, int length) : _payload(std::make_shared<PackerContent>(payload)),
-                                                     Length(length), Payload(_payload->Buffer) {}
+    RTypeNetworkPayload(char *payload, int length) : _payload(std::make_shared<PackerContent>(payload, length)),
+                                                     Length(length), Payload(_payload->_buffer) {}
 
-    RTypeNetworkPayload(int length) : _payload(std::make_shared<PackerContent>(length)), Length(length), Payload(_payload->Buffer) { }
+    RTypeNetworkPayload(int length) : _payload(std::make_shared<PackerContent>(length)), Length(length), Payload(_payload->_buffer) { }
 
     RTypeNetworkPayload(char *payload, int length, std::string const &destination) : Ip(destination),
-                                                                                     _payload(std::make_shared<PackerContent>(payload)),
+                                                                                     _payload(std::make_shared<PackerContent>(payload, length)),
                                                                                      Length(length),
-                                                                                     Payload(_payload->Buffer){ }
+                                                                                     Payload(_payload->_buffer){ }
 
     RTypeNetworkPayload(const RTypeNetworkPayload &payload) : Ip(payload.Ip),  _payload(payload._payload),
                                                               Length(payload.Length),
-                                                              Payload(_payload->Buffer){ }
+                                                              Payload(_payload->_buffer){ }
 
-    RTypeNetworkPayload(const RType::Packer &packer) : _payload(std::make_shared<PackerContent>(packer.getBuffer())), Length(packer.getLength()),
-                                                       Payload(_payload->Buffer){ }
+    RTypeNetworkPayload(const RType::Packer &packer) : _payload(std::make_shared<PackerContent>(packer.getBuffer(), packer.getLength())), Length(packer.getLength()),
+                                                       Payload(_payload->_buffer){ }
 
-    RTypeNetworkPayload(const RType::Packer &packer, std::string const &destination) : _payload(std::make_shared<PackerContent>(packer.getBuffer())),
+    RTypeNetworkPayload(const RType::Packer &packer, std::string const &destination) : _payload(std::make_shared<PackerContent>(packer.getBuffer(), packer.getLength())),
                                                                                        Length(packer.getLength()),
-                                                                                       Payload(_payload->Buffer){ }
+                                                                                       Payload(_payload->_buffer){ }
 
     const RTypeNetworkPayload &operator=(const RTypeNetworkPayload &payload) { return *this; }
 };
