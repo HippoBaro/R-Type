ff934fda1da67b8cf56ae997611ec004a61b938d
HippoBaro <hippolyte.barraud@epitech.eu>
2017-01-01 05:56:25 +0100

    pipe gcloud installation output to null so that it doesn't spam stdout

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ openssl aes-256-cbc -K $encrypted_482e2f17cfc1_key -iv $encrypted_482e2f17cfc1_i
 tar -xzf credentials.tar.gz;
 
 gcloud version || true
-if [ ! -d "$HOME/google-cloud-sdk/bin" ]; then rm -rf $HOME/google-cloud-sdk; export CLOUDSDK_CORE_DISABLE_PROMPTS=1; curl https://sdk.cloud.google.com | bash; fi
+if [ ! -d "$HOME/google-cloud-sdk/bin" ]; then rm -rf $HOME/google-cloud-sdk; export CLOUDSDK_CORE_DISABLE_PROMPTS=1; curl https://sdk.cloud.google.com | bash > /dev/null; fi
 source /home/travis/google-cloud-sdk/path.bash.inc
 gcloud version
 gcloud auth activate-service-account --key-file RType-server-736393d1bca7.json;
