f9b60375e219b5aa174a5ae8ec3336ed59fe7a96
HippoBaro <hippolyte.barraud@epitech.eu>
2017-01-01 05:32:20 +0100

    Added noninteractive export and -y to apt-get packet install commands in VM

--- install_run_server.sh
+++ install_run_server.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+export DEBIAN_FRONTEND=noninteractive
+
 echo "Updating packages"
 sudo apt-get update
 echo "Installing dependencies"
-sudo apt-get install gcc-5 g++-5 cmake
+sudo apt-get install -y gcc-5 g++-5 cmake
 export CXX="g++-5" CC="gcc-5"
 echo "Building project"
 mkdir build && cd build
