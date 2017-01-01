5c7f3096f5df443eb7f101e95e04554236a37940
HippoBaro <hippolyte.barraud@epitech.eu>
2017-01-01 05:14:18 +0100

    Updating gcloud before deploying

--- deploy.sh
+++ deploy.sh
@@ -3,11 +3,13 @@
 set -ev
 
 openssl aes-256-cbc -K $encrypted_482e2f17cfc1_key -iv $encrypted_482e2f17cfc1_iv -in credentials.tar.gz.enc -out credentials.tar.gz -d;
-if [ ! -d ${HOME}/google-cloud-sdk ]; then curl https://sdk.cloud.google.com | bash ; fi ;
 tar -xzf credentials.tar.gz;
-gcloud auth activate-service-account --key-file RType-server-736393d1bca7.json;
 
-gcloud components update;
+gcloud version || true
+if [ ! -d "$HOME/google-cloud-sdk/bin" ]; then rm -rf $HOME/google-cloud-sdk; export CLOUDSDK_CORE_DISABLE_PROMPTS=1; curl https://sdk.cloud.google.com | bash; fi
+source /home/travis/google-cloud-sdk/path.bash.inc
+gcloud version
+gcloud auth activate-service-account --key-file RType-server-736393d1bca7.json;
 
 gcloud config set project rtype-server;
 gcloud compute --project "rtype-server" ssh --zone "europe-west1-c" "rtype-server-dev" --quiet --command="rm -rf R-Type";
