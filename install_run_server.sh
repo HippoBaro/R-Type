c02772341119541b80634ea2aa6df59686ef94ca
HippoBaro <hippolyte.barraud@epitech.eu>
2017-01-01 02:38:04 +0100

    Refactored Travis file & created install script

--- install_run_server.sh
+++ install_run_server.sh
@@ -1,15 +1,7 @@
 #!/bin/bash
 
-echo "Updating packages"
 sudo apt-get update
-echo "Installing dependencies"
 sudo apt-get install gcc-5 g++-5 cmake
-export CXX="g++-5" CC="gcc-5"
-echo "Building project"
-mkdir build && cd build
-cmake -D RTYPE_BUILD_SERVER_ONLY=1 ..
+mkdir build && cd build && cmake ..
 make
-echo "Killing current server instance if any"
-sudo pkill -f R_Type_Server
-echo "Launching"
 ./R_Type_Server
