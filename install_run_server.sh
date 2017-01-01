5ed001ac6321c2549fa8cbf809437bc6d4486ca9
HippoBaro <hippolyte.barraud@epitech.eu>
2017-01-01 03:19:15 +0100

    Exported gcc & g++ in GCE

--- install_run_server.sh
+++ install_run_server.sh
@@ -4,6 +4,7 @@ echo "Updating packages"
 sudo apt-get update
 echo "Installing dependencies"
 sudo apt-get install gcc-5 g++-5 cmake
+export CXX="g++-5" CC="gcc-5"
 echo "Building project"
 mkdir build && cd build
 cmake -D RTYPE_BUILD_SERVER_ONLY=1 ..
