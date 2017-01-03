7cda99396c8d90ba08d3c2f2114f1343014dc50e
aguado_e <enzo.aguado@epitech.eu>
2017-01-03 06:56:39 +0100

    fix constructor for std::chrono

--- SharedLibs/Entities/Player/Player.cpp
+++ SharedLibs/Entities/Player/Player.cpp
@@ -18,7 +18,7 @@ Player::Player(const std::initializer_list<void *> init) : Player(*GetParamFromI
 Player::Player(uint16_t id, std::shared_ptr<Timer> timer, std::shared_ptr<RType::EventManager> eventManager, TimeRef const &timeRef, vec2<float> const &startPosition) :
         Entity(id, timer, eventManager),
         _currentPosition(startPosition),
-        _shotCooldown(std::chrono::high_resolution_clock::now())
+        _shotCooldown(std::chrono::steady_clock::now())
 {
     auto now = _timer->getCurrent();
     _partition = EntityPartitionBuilder(_timer, now, startPosition).AddSegment(
@@ -82,11 +82,11 @@ vec2<float> Player::getVectorFromInput(std::set<UserEventType> &events) {
         direction = vec2<float>(-velocity, 0);
     if (events.count(USER_SPACE) > 0)
     {
-        auto now = std::chrono::system_clock::now();
-        auto duration = std::chrono::duration<double, std::milli>(now - _shotCooldown).count();
+        auto now = std::chrono::steady_clock::now();
+        auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(now - _shotCooldown).count();
         if (duration > 500)
         {
-            _shotCooldown = std::chrono::high_resolution_clock::now();
+            _shotCooldown = std::chrono::steady_clock::now();
             _shouldFire = true;
         }
     }
